13e5964cfdaecf850acf8e99065ab59641afb598
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2024-02-19 21:35:24 +0100

    Graceful shutdown of port monitors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tokio::io::AsyncReadExt;
 use tokio::signal;
+use tokio_util::sync::CancellationToken;
 
 mod read_line;
 
@@ -7,7 +8,7 @@ const DEFAULT_SERIAL_PORT: &str = "/dev/ttyUSB0";
 const DEFAULT_BAUD_RATE: u32 = 115200;
 const DEFAULT_BUFFER_SIZE: usize = 4096;
 
-async fn monitor_port(port_name: &String) {
+async fn monitor_port(port_name: &String, cancel_token: CancellationToken) {
     let port_builder = tokio_serial::new(port_name, DEFAULT_BAUD_RATE);
     let mut serial_stream = match tokio_serial::SerialStream::open(&port_builder) {
         Ok(s) => s,
@@ -21,23 +22,31 @@ async fn monitor_port(port_name: &String) {
     let mut process_buffer = vec![];
 
     loop {
-        let read_result = serial_stream.read(&mut recv_buffer).await;
-        match read_result {
-            Ok(n) => {
-                if n == 0 {
-                    continue;
-                }
-
-                // concatenate the new data to the process buffer
-                process_buffer.extend_from_slice(&recv_buffer[0..n]);
-
-                while let Ok(line) = read_line::read_line_from_buffer(&mut process_buffer) {
-                    print!("{}\t| {}", port_name, line);
-                }
-            }
-            Err(e) => {
-                println!("Failed to read from serial port: {}", e);
+        tokio::select! {
+            _ = cancel_token.cancelled() => {
+                println!("Closing {} port monitor", port_name);
                 return;
+            }
+
+            read_result = serial_stream.read(&mut recv_buffer) => {
+                match read_result {
+                    Ok(n) => {
+                        if n == 0 {
+                            continue;
+                        }
+
+                        // concatenate the new data to the process buffer
+                        process_buffer.extend_from_slice(&recv_buffer[0..n]);
+
+                        while let Some(line) = read_line::read_line_from_buffer(&mut process_buffer) {
+                            print!("{}\t| {}", port_name, line);
+                        }
+                    }
+                    Err(e) => {
+                        println!("Failed to read from serial port: {}", e);
+                        return;
+                    }
+                }
             }
         }
     }
@@ -58,17 +67,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Starting to read from serial ports. Press CTRL-C to exit.");
 
+    let mut handles = Vec::new();
+    let cancel_signal = CancellationToken::new();
+
     for port_name in port_names {
         let port_name_clone = port_name.clone();
-        tokio::spawn(async move { monitor_port(&port_name_clone).await });
+        let cancel_signal_clone = cancel_signal.clone();
+        let handle =
+            tokio::spawn(async move { monitor_port(&port_name_clone, cancel_signal_clone).await });
+        handles.push(handle);
     }
 
     loop {
         tokio::select! {
             _ = signal::ctrl_c() => {
                 println!(">> Received CTRL-C. Exiting...");
-                return Ok(());
+
+                cancel_signal.cancel();
+                break;
             }
         }
     }
+
+    for handle in handles {
+        handle.await?;
+    }
+
+    Ok(())
 }

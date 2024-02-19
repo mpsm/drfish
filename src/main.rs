fc34103ea750b577887153d44d008327dcdae2fe
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2024-02-19 20:48:13 +0100

    Monitor many ports line by line

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,70 @@
 use tokio::io::AsyncReadExt;
 use tokio::signal;
 
+mod read_line;
+
 const DEFAULT_SERIAL_PORT: &str = "/dev/ttyUSB0";
 const DEFAULT_BAUD_RATE: u32 = 115200;
+const DEFAULT_BUFFER_SIZE: usize = 4096;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("DrFish is a fish doctor! 🐟");
-
-    let port_name = std::env::args().nth(1).unwrap_or_else(|| {
-        println!(
-            "No serial port name provided. Using default: {}",
-            DEFAULT_SERIAL_PORT
-        );
-        DEFAULT_SERIAL_PORT.to_string()
-    });
-
+async fn monitor_port(port_name: &String) {
     let port_builder = tokio_serial::new(port_name, DEFAULT_BAUD_RATE);
     let mut serial_stream = match tokio_serial::SerialStream::open(&port_builder) {
         Ok(s) => s,
         Err(e) => {
             println!("Failed to open serial port: {}", e);
-            return Ok(());
+            return;
         }
     };
 
-    let mut buffer = vec![0; 1024];
+    let mut recv_buffer = vec![0; DEFAULT_BUFFER_SIZE];
+    let mut process_buffer = vec![];
 
-    println!("Starting to read from serial port. Press CTRL-C to exit.");
+    loop {
+        let read_result = serial_stream.read(&mut recv_buffer).await;
+        match read_result {
+            Ok(n) => {
+                if n == 0 {
+                    continue;
+                }
+
+                // concatenate the new data to the process buffer
+                process_buffer.extend_from_slice(&recv_buffer[0..n]);
+
+                while let Ok(line) = read_line::read_line_from_buffer(&mut process_buffer) {
+                    print!("{}\t| {}", port_name, line);
+                }
+            }
+            Err(e) => {
+                println!("Failed to read from serial port: {}", e);
+                return;
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("DrFish is a fish doctor! 🐟");
+
+    let mut port_names = std::env::args().skip(1).collect::<Vec<_>>();
+    if port_names.is_empty() {
+        println!(
+            "No serial port name provided. Using default: {}",
+            DEFAULT_SERIAL_PORT
+        );
+        port_names.push(DEFAULT_SERIAL_PORT.to_string());
+    }
+
+    println!("Starting to read from serial ports. Press CTRL-C to exit.");
+
+    for port_name in port_names {
+        let port_name_clone = port_name.clone();
+        tokio::spawn(async move { monitor_port(&port_name_clone).await });
+    }
 
     loop {
         tokio::select! {
-            _ = serial_stream.read(&mut buffer) => {
-                let s = String::from_utf8_lossy(&buffer);
-                println!(">> Received: {}", s);
-            }
-
             _ = signal::ctrl_c() => {
                 println!(">> Received CTRL-C. Exiting...");
                 return Ok(());
---
+++ src/read_line.rs
@@ -0,0 +1,61 @@
+use std::io::{Error, ErrorKind};
+
+pub trait AsyncReadLine {
+    async fn read_line(&mut self) -> String;
+}
+
+pub fn read_line_from_buffer(buffer: &mut Vec<u8>) -> Result<String, Error> {
+    if let Some(i) = buffer.iter().position(|&x| x == b'\n') {
+        let line = String::from_utf8_lossy(&buffer[..=i]).to_string();
+        buffer.drain(..=i);
+        Ok(line)
+    } else {
+        Err(Error::new(ErrorKind::Other, "No newline found"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_line_from_buffer_single_line() {
+        let mut buffer = "hello\n".as_bytes().to_vec();
+        let line = read_line_from_buffer(&mut buffer).unwrap();
+        assert_eq!(line, "hello\n");
+        assert!(buffer.is_empty());
+    }
+
+    #[test]
+    fn test_read_line_from_buffer_multiple_lines() {
+        let mut buffer = "hello\nworld\n".as_bytes().to_vec();
+        let line = read_line_from_buffer(&mut buffer).unwrap();
+        assert_eq!(line, "hello\n");
+        assert_eq!(buffer, "world\n".as_bytes().to_vec());
+    }
+
+    #[test]
+    fn test_read_line_from_buffer_no_newline() {
+        let mut buffer = "hello".as_bytes().to_vec();
+        let result = read_line_from_buffer(&mut buffer);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_read_line_from_buffer_split_line() {
+        let mut buffer1 = "hello\nworld".as_bytes().to_vec();
+        let line1 = read_line_from_buffer(&mut buffer1).unwrap();
+        assert_eq!(line1, "hello\n");
+        assert_eq!(buffer1, "world".as_bytes().to_vec());
+
+        let mut buffer2 = " part2\nend\n".as_bytes().to_vec();
+        buffer2.splice(0..0, buffer1); // prepend remaining data from buffer1 to buffer2
+        let line2 = read_line_from_buffer(&mut buffer2).unwrap();
+        assert_eq!(line2, "world part2\n");
+        assert_eq!(buffer2, "end\n".as_bytes().to_vec());
+
+        let line3 = read_line_from_buffer(&mut buffer2).unwrap();
+        assert_eq!(line3, "end\n");
+        assert!(buffer2.is_empty());
+    }
+}

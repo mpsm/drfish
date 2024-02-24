938898104a90b30b13ea3f63dc8725989d366ed5
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2024-02-24 15:47:34 +0100

    Reading from stdin PoC

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use chrono;
 use std::fs::OpenOptions;
+use std::io::Error;
 use std::io::Write;
-use tokio::signal;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 use tokio_util::sync::CancellationToken;
 
 mod cli;
@@ -12,29 +14,52 @@ mod serial_monitor;
 
 use log_monitor::AsyncLogMonitor;
 
+/// Asynchronously gets single key from the user.
+async fn get_key(
+    stdin: &mut termion::input::Keys<termion::AsyncReader>,
+) -> Result<termion::event::Key, Error> {
+    loop {
+        match stdin.next() {
+            Some(c) => {
+                return c;
+            }
+            None => {
+                // wait for the next key
+                tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("DrFish is a fish doctor! 🐟");
 
-    let port_configuration = cli::get_port_configuration();
-    match port_configuration {
-        Ok(_) => {}
+    let port_configuration = match cli::get_port_configuration() {
+        Ok(cfg) => cfg,
         Err(e) => {
             println!("Error: {}", e);
             std::process::exit(1);
         }
-    }
+    };
 
     println!("Starting to read from serial ports. Press CTRL-C to exit.");
 
     let mut handles = Vec::new();
     let cancel_signal = CancellationToken::new();
-    let (sender, mut receiver) = tokio::sync::mpsc::unbounded_channel::<log_monitor::Log>();
+    let (sender, mut receiver) =
+        tokio::sync::mpsc::unbounded_channel::<log_monitor::MonitorMessage>();
 
-    for port in port_configuration.unwrap() {
+    // map to store the write proxies for each port
+    let mut write_proxies = std::collections::HashMap::new();
+
+    for port in &port_configuration {
         let cancel_signal_clone = cancel_signal.clone();
         let sender_clone = sender.clone();
-        let mut port_monitor = serial_monitor::SerialLogMonitor::new(port)?;
+        let mut port_monitor = serial_monitor::SerialLogMonitor::new(port.clone())?;
+        let write_proxy = port_monitor.get_write_proxy();
+        write_proxies.insert(port_monitor.get_common_name(), write_proxy);
+
         let handle = tokio::spawn(async move {
             port_monitor
                 .monitor(cancel_signal_clone, sender_clone)
@@ -42,6 +67,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
         handles.push(handle);
     }
+
+    let active_writer = &port_configuration[0];
 
     let timestamp = chrono::Local::now().format("%Y-%m-%d_%H-%M-%S");
     let log_file_name = format!("log_{}.txt", timestamp);
@@ -51,20 +78,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .append(true)
         .open(&log_file_name)?;
 
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
+    let mut stdin = termion::async_stdin().keys();
+
+    let mut last_msg_was_unsolicited = false;
+
     loop {
         tokio::select! {
-            _ = signal::ctrl_c() => {
-                println!(">> Received CTRL-C. Exiting...");
+            msg = receiver.recv() => {
+                if let Some(msg) = msg {
+                    match msg {
+                        log_monitor::MonitorMessage::UnsolictedMessage(msg) => {
+                            print!("{}", msg);
+                            stdout.flush()?;
+                            last_msg_was_unsolicited = true;
+                        }
+                        log_monitor::MonitorMessage::Log(msg) => {
+                            if last_msg_was_unsolicited {
+                                print!("\r\n");
+                            }
+                            let log_msg = format!(">> [{}] | {}: {}\r\n", msg.timestamp, msg.source_name, msg.message);
+                            print!("{}", &log_msg);
+                            write!(log_file, "{}", &log_msg)?;
+                            stdout.flush()?;
+                            last_msg_was_unsolicited = false;
+                        }
 
-                cancel_signal.cancel();
+                    }
+                }
+            }
+
+            _ = tokio::signal::ctrl_c() => {
+                print!("Captured Ctrl+c, bye fisherman!\r\n");
                 break;
             }
 
-            msg = receiver.recv() => {
-                if let Some(msg) = msg {
-                    let log_msg = format!(">> [{}] | {}: {}", msg.timestamp, msg.source_name, msg.message);
-                    print!("{}", &log_msg);
-                    write!(log_file, "{}", &log_msg)?;
+            key = get_key(&mut stdin) => {
+                match key {
+                    Ok(termion::event::Key::Ctrl('x')) => {
+                        print!("Exiting...\r\n");
+                        cancel_signal.cancel();
+                        break;
+                    }
+                    Ok(c) => {
+                        let writer_proxy = write_proxies.get(&active_writer.path).unwrap();
+                        match c {
+                            termion::event::Key::Char(c) => {
+                                // FIXME: this is a hack to send CRLF to the serial port
+                                if c == '\n' {
+                                    writer_proxy.send('\r' as u8);
+                                    continue;
+                                }
+
+                                writer_proxy.send(c as u8);
+                            }
+                            _ => {}
+                        }
+                    }
+                    Err(e) => {
+                        print!("Error: {}\r\n", e);
+                    }
                 }
             }
         }
--- src/serial_monitor.rs
+++ src/serial_monitor.rs
@@ -1,32 +1,67 @@
+use crate::log_monitor;
+
 use super::data::SerialPortSettings;
-use super::log_monitor::{AsyncLogMonitor, Log};
+use super::log_monitor::{AsyncLogMonitor, Log, MonitorMessage};
 use super::read_line;
 
 use chrono;
-use tokio::io::AsyncReadExt;
-use tokio::sync::mpsc::UnboundedSender;
-use tokio_serial::SerialStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio_serial::{SerialPort, SerialStream};
 use tokio_util::sync::CancellationToken;
 
-const DEFAULT_BUFFER_SIZE: usize = 4096;
+const DEFAULT_BUFFER_SIZE: usize = 128;
+// TODO: depends on a baud rate
+const BUFFER_COMPLETION_TIMEOUT: u64 = 50;
+const IO_TIMEOUT: u64 = 10;
 
 pub struct SerialLogMonitor {
     port_settings: SerialPortSettings,
     serial_stream: SerialStream,
+    write_receiver: UnboundedReceiver<u8>,
+    write_sender: UnboundedSender<u8>,
+}
+
+pub struct SerialLogMonitorWriteProxy {
+    write_sender: UnboundedSender<u8>,
+}
+
+impl SerialLogMonitorWriteProxy {
+    pub fn new(write_sender: UnboundedSender<u8>) -> SerialLogMonitorWriteProxy {
+        SerialLogMonitorWriteProxy {
+            write_sender: write_sender,
+        }
+    }
+
+    pub fn send(&self, data: u8) {
+        self.write_sender.send(data).unwrap();
+    }
 }
 
 impl SerialLogMonitor {
     pub fn new(port_settings: SerialPortSettings) -> Result<SerialLogMonitor, String> {
         let port_builder = tokio_serial::new(port_settings.path.clone(), port_settings.baud_rate);
-        let serial_stream = match tokio_serial::SerialStream::open(&port_builder) {
+        let mut serial_stream = match tokio_serial::SerialStream::open(&port_builder) {
             Ok(s) => s,
             Err(e) => return Err(format!("Failed to open serial port: {}", e)),
         };
 
+        serial_stream
+            .set_timeout(std::time::Duration::from_millis(IO_TIMEOUT))
+            .map_err(|e| format!("Failed to set serial port timeout: {}", e))?;
+
+        let (write_sender, write_receiver) = tokio::sync::mpsc::unbounded_channel::<u8>();
+
         Ok(SerialLogMonitor {
             port_settings: port_settings,
             serial_stream: serial_stream,
+            write_receiver: write_receiver,
+            write_sender: write_sender,
         })
+    }
+
+    pub fn get_write_proxy(&self) -> SerialLogMonitorWriteProxy {
+        SerialLogMonitorWriteProxy::new(self.write_sender.clone())
     }
 }
 
@@ -38,26 +73,44 @@ impl AsyncLogMonitor for SerialLogMonitor {
     async fn monitor(
         &mut self,
         cancel_token: CancellationToken,
-        sender_queue: UnboundedSender<Log>,
+        sender_queue: UnboundedSender<MonitorMessage>,
     ) {
         let mut recv_buffer = vec![0; DEFAULT_BUFFER_SIZE];
         let mut process_buffer = vec![];
 
-        println!(
-            "Starting {} port monitor @ {}",
+        print!(
+            "Starting {} port monitor @ {}\r\n",
             self.port_settings.path, self.port_settings.baud_rate
         );
 
         loop {
             tokio::select! {
                 _ = cancel_token.cancelled() => {
-                    println!("Closing {} port monitor", self.port_settings.path);
                     return;
                 }
 
-                read_result = self.serial_stream.read(&mut recv_buffer) => {
+                write_result = self.write_receiver.recv() => {
+                    match write_result {
+                        Some(data) => {
+                            match self.serial_stream.write(&[data]).await {
+                                Ok(_) => {}
+                                Err(_) => {
+                                    continue;
+                                }
+                            }
+                        }
+                        None => {
+                            continue;
+                        }
+                    }
+                }
+
+                read_result = tokio::time::timeout(
+                    std::time::Duration::from_millis(BUFFER_COMPLETION_TIMEOUT),
+                    self.serial_stream.read(&mut recv_buffer),
+                ) => {
                     match read_result {
-                        Ok(n) => {
+                        Ok(Ok(n)) => {
                             if n == 0 {
                                 continue;
                             }
@@ -66,17 +119,27 @@ impl AsyncLogMonitor for SerialLogMonitor {
                             process_buffer.extend_from_slice(&recv_buffer[0..n]);
 
                             while let Some(line) = read_line::read_line_from_buffer(&mut process_buffer) {
+                                let stripped_line = line.trim().to_string();
                                 let message = Log {
                                     source_name: self.get_common_name(),
-                                    message: line,
+                                    message: stripped_line,
                                     timestamp: chrono::Local::now(),
                                 };
-                                sender_queue.send(message).unwrap();
+                                sender_queue.send(MonitorMessage::Log(message)).unwrap();
                             }
                         }
-                        Err(e) => {
-                            println!("Failed to read from serial port: {}", e);
-                            return;
+                        // FIXME: handle the error
+                        Ok(Err(_)) => {
+                            continue;
+                        }
+                        Err(_) => {
+                            if process_buffer.len() == 0 {
+                                continue;
+                            }
+
+                            let unsolicted_msg = String::from_utf8_lossy(process_buffer.as_slice()).to_string();
+                            process_buffer.clear();
+                            sender_queue.send(log_monitor::MonitorMessage::UnsolictedMessage(unsolicted_msg)).unwrap();
                         }
                     }
                 }
